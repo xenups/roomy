493acf1fa5f6a8e3c83bf35c45308c3eaf251376
xenups <xenups@outlook.com>
2022-06-12 15:55:40 +0430

    init

--- listing/urls.py
+++ listing/urls.py
@@ -5,10 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('listings/', get_owner_listings_controller, name='listings'),
     path('reserve/', make_reservation_controller, name='reserve'),
-    path('rooms/available/', get_available_rooms_controller, name='reserve'),
-    path('menu/', owner_listing_view),
-    # path('listing_rooms/', method_dispatch(GET=get_listing_rooms), name='y'),
-    # path('available_listings/', method_dispatch(GET=get_available_listings), name='t'),
-    # path('available_rooms/', method_dispatch(GET=get_available_rooms_controller), name='t'),
-
+    path('rooms/available/', get_available_rooms_controller, name='available'),
+    path('manager/', owner_listing_view),
 ]
